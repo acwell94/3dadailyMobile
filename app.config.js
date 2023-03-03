485560d864e36f2e91e771d41fb0097111881e12
acwell94 <acwell94@naver.com>
2023-03-04 00:01:55 +0900

    230303 배포 수정

--- app.config.js
+++ app.config.js
@@ -13,6 +13,10 @@ module.exports = {
       "android.permission.READ_EXTERNAL_STORAGE",
       "android.permission.WRITE_EXTERNAL_STORAGE",
     ],
+    adaptiveIcon: {
+      foregroundImage: "./assets/icon.png",
+      backgroundColor: "#FFFFFF",
+    },
     config: {
       googleMaps: {
         apiKey: process.env.GOOGLE_API_KEY,
